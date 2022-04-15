6d451a02a9b289c5789030fa47dd65e71d08ec14
Jeko Tchelidze <jekotchelidze@gmail.com>
2022-04-15 17:49:55 +0400

    com

---
+++ assets/js/main.js
@@ -0,0 +1,13 @@
+$('.main-slider').slick({
+    nextArrow: false,
+    prevArrow: false,
+    // infinite: true,
+    prevArrow:false,
+    nextArrow:false,
+    dots: true
+  });
+
+  
+  $(document).ready(function() {
+    $(".slick-dots li button").text(" ");
+ });
