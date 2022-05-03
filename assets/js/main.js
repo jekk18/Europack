7fa437e5526c84538917a3bb2dba07bcc5d4cc28
Jeko Tchelidze <jekotchelidze@gmail.com>
2022-05-03 18:41:11 +0400

    c

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,7 +8,15 @@ $('.main-slider').slick({
     autoplay:true,
     autoplaySpeed:4000
   });
-
+  $('.news-slider').slick({
+    prevArrow:$('#prev'),
+    nextArrow:$('#next'),
+    // infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay:true,
+    autoplaySpeed:4000
+  });
   
   $(document).ready(function() {
     $(".slick-dots li button").text(" ");
@@ -18,4 +26,16 @@ $('.main-slider').slick({
  $(document).on('click', '.bottom-navbar ul li ', function(){
    console.log("click")
   $(this).addClass('colored').siblings().removeClass('colored');
+});
+
+$('.count').each(function () {
+  $(this).prop('Counter',0).animate({
+      Counter: $(this).text()
+  }, {
+      duration: 4000,
+      easing: 'swing',
+      step: function (now) {
+          $(this).text(Math.ceil(now));
+      }
+  });
 });
